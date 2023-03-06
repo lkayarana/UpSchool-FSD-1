57c0869231fc3863c990b5bd95125e14df47072c
Rana Kaya <102520514+lkayarana@users.noreply.github.com>
2023-03-06 20:26:34 +0300

    Add files via upload

---
+++ HOMEWORKS/Ödev0/Homework1/RandomPasswordGenerator/RandomPasswordGenerator/Inputs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPasswordGenerator
+{
+    public static class Inputs
+    {
+        public static class Questions 
+        {
+            public static string IncludeNumbers => "Do you want to include numbers?";
+
+            public static string IncludeLowercaseCharacters => "OK! How about lowercase characters?";
+
+            public static string IncludeUppercaseCharacters => "Very nice! How about uppercase characters?";
+
+            public static string IncludeSpecialCharachters => "All right! We are almost done. Would you also like to add speacial characters?";
+
+            public static string PasswordLength => "Great! Lastly, how long do you want to keep ypur password lenght?";
+
+        }
+
+    }
+}
