508b0901eb041360993dc33afd2ed106e9aeef8c
Rana Kaya <102520514+lkayarana@users.noreply.github.com>
2023-06-23 19:21:51 +0300

    Updated

---
+++ HOMEWORKS/Ödev1/RandomPasswordGenerator/Inputs.cs
@@ -0,0 +1,16 @@
+﻿namespace RandomPasswordGenerator
+{
+    public static class Inputs
+    {
+        public static class Questions
+        {
+            public static string IncludeNumbers => "Do you want to include Numbers?";
+            public static string IncludeLowercaseCharacters => "OK! How about lowercase characters?";
+            public static string IncludeUppercaseCharacters => "Very nice! How about uppercase characters?";
+            public static string IncludeSpecialCharacters => "Would you like to add special characters?";
+            public static string PasswordLength => "Finally, how long do you want to keep your password length?";
+
+        }
+
+    }
+}
---
+++ HOMEWORKS/Ödev1/RandomPasswordGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using RandomPasswordGenerator;
+
+var passwordGenerator = new PasswordGenerator();
+
+passwordGenerator.Introduction();
+
+passwordGenerator.ReadInputs();
+
+passwordGenerator.Create();
+
+passwordGenerator.WriteLatestGeneratedPassword();
+
+Console.ReadLine();
+
+return 0;
