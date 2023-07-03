ee2f7b2a16fb8bf25e34379ce7e3bb24c5765f99
Rana Kaya <102520514+lkayarana@users.noreply.github.com>
2023-07-03 19:15:54 +0300

    upgrade

---
+++ HOMEWORKS/Ödev1/RandomPasswordGenerator/RandomPasswordGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using RandomPasswordGenerator;
+using System;
+
+var passwordGenerator = new PasswordGenerator;
+
+passwordGenerator.Greetings();
+
+passwordGenerator.ReadInputs();
+
+passwordGenerator.Generate();
+
+passwordGenerator.WriteLatestGeneratedPassword();
+
+Console.ReadLine();
+
+return 0;
